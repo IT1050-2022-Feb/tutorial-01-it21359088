e63181013463a5fe7171da50b894ea355b06c6f9
IT21359088 Raveesha <99808540+it21359088@users.noreply.github.com>
2022-02-16 14:02:28 +0000

    tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
+  float x,y,z;
+
+  printf("Input marks of subject 1: ");
+  scanf("%f", &x);
+
+  printf("Input marks of subject 2: ");
+  scanf("%f", &y);
+
+  z=(float)(x+y)/2;
+  printf("average: %f", z);
   return 0;
 }
 
